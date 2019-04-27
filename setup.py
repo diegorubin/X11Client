fac0425b8e0e11a6ea6691401a1340569b128b3f
Diego Rubin <rubin.diego@gmail.com>
2019-04-27 09:52:30 -0300

    fix setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, Extension, find_packages
 
 module = Extension('x11_client',
                    define_macros=[('MAJOR_VERSION', '1'),
-                                  ('MINOR_VERSION', '3')],
+                                  ('MINOR_VERSION', '4')],
                    include_dirs=['/usr/include', 'ext'],
                    libraries=['X11', 'Xtst'],
                    library_dirs=['/usr/lib'],
-                   sources=['ext/x11_client.c', 'ext/x11_client.h'])
+                   headers=['ext/x11_client.h'],
+                   sources=['ext/x11_client.c'])
 
 setup(name='X11Client',
-      version='1.3',
+      version='1.4',
       description='X11 client',
       author='Diego Rubin',
       author_email='rubin.diego@gmail.com',
